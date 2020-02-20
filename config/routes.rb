0aedb999f26bcb3dcdee2ae46ff01f4539e0bad2
earthctzn <earthctzn1@gmail.com>
2020-02-20 16:13:50 -0500

    Adding preliminary routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,20 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+
+  namespace :api do
+    namespace :v1 do
+      resources :breweries only: [:index, :create]
+      resources :reviews
+    end
+  end
+
+  
+  post 'api/v1/login' => 'sessions#login'
+  post 'api/v1/logout' => 'sessions#logout'
+  post 'api/v1/signup' => 'users#create'
+  
+
+  get '/auth/facebook/callback', to: 'sessions#fbauth'
+  get 'auth/failure', to: redirect('http://localhost:5000/login')
+
 end
