d8498530a7090d607613af661ba4e5fb8c2c6d8e
earthctzn <earthctzn1@gmail.com>
2020-03-06 16:22:15 -0500

    Removes unused facebook login code

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -8,7 +8,7 @@ Rails.application.config.action_controller.forgery_protection_origin_check = fal
 
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    origins 'https://localhost:5000'
+    origins 'http://localhost:5000'
 
     resource '*',
       headers: :any,
