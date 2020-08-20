cfb9fd927320b39bf92d4d755fda73997d8347c9
earthctzn <earthctzn1@gmail.com>
2020-08-20 04:03:46 -0400

    Tests force_ssl.

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -4,6 +4,7 @@
 # Handle Cross-Origin Resource Sharing (CORS) in order to accept cross-origin AJAX requests.
 
 # Read more: https://github.com/cyu/rack-cors
+Rails.application.config.session_store :cookie_store, secure: Rails.env.production?
 Rails.application.config.action_controller.forgery_protection_origin_check = false
 
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
