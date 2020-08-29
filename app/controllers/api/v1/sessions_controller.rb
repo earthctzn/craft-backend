0ed8dc519fcd1eb71d0490659fd2af80c09a5d2b
earthctzn <earthctzn1@gmail.com>
2020-08-28 20:43:29 -0400

    tests conditional in get user action.

--- app/controllers/api/v1/sessions_controller.rb
+++ app/controllers/api/v1/sessions_controller.rb
@@ -19,6 +19,8 @@ class Api::V1::SessionsController < ApplicationController
         user = User.find_by(id: session[:user_id])
         if user
             render json: user, only: [:id, :username], include: [:reviews], status: :ok
+        else
+            return
         end
     end
 
